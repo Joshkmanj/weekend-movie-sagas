873f319821613459e62d21b7d08d5b614e9d9541
Josh Kralewski <joshkmanj@gmail.com>
2022-03-06 19:07:30 -0600

    Genre tags now appending to DOM, css grid adjusted to fit

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,17 +1,33 @@
 // import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './Details.css'
 
 function Details(){
 
     const movie = useSelector(store =>store.selectedMovie)
+    const history = useHistory()
+
+
+    const handleClick = () =>{
+        history.push('/') // On click of return button, this brings the user to the home page
+    }
 
     return(
         <div className="big-card">
             <img src={movie.poster} alt={movie.title}/>
             <h2>{movie.title}</h2>
             <p>{movie.description}</p>
-            <button>return</button>
+            <div className='tag-holder'>
+            {(movie.genres).map(genre =>(
+                <p
+                key={genre.name} 
+                className='genre-tag'>{genre.name}</p>
+                ))}
+            </div>
+            <button
+            onClick={handleClick}
+            >return</button>
         </div>
     )
 }
